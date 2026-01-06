3908c54338808d7ac49556de0ff248f3f8213576
abrosenkov <abrosenkov91@gmail.com>
2026-01-07 00:43:21 +0100

    add header and home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,20 @@
+import Link from "next/link";
 import css from "./page.module.css";
 
 export default function Home() {
-  return <main className={css.main}></main>;
+  return (
+    <main className={css.main}>
+      <div className={css.hero}>
+        <div className="container">
+          <h1 className={css.title}>Campers of your dreams</h1>
+          <p className={css.description}>
+            You can find everything you want in our catalog
+          </p>
+          <Link className={css.ctaButton} href="/catalog">
+            View Now
+          </Link>
+        </div>
+      </div>
+    </main>
+  );
 }
--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -1,5 +1,47 @@
+"use client";
+
+import Link from "next/link";
 import css from "./Header.module.css";
+import clsx from "clsx";
+import { usePathname } from "next/navigation";
 
 export default function Header() {
-  return <header className={css.header}></header>;
+  const pathname = usePathname();
+
+  const isHome = pathname === "/";
+  const isCatalog = pathname.startsWith("/catalog");
+
+  return (
+    <header className={css.header}>
+      <div className={`${css.navWrapper} container`}>
+        <Link href="/" aria-label="Home">
+          <svg className={css.logo}>
+            <use href="/sprite.svg#logo" />
+          </svg>
+        </Link>
+        <nav className={css.menu} aria-label="Main Navigation">
+          <ul className={css.navigation}>
+            <li>
+              <Link
+                className={clsx(css.link, isHome && css.active)}
+                href="/"
+                aria-label="Home"
+              >
+                Home
+              </Link>
+            </li>
+            <li>
+              <Link
+                className={clsx(css.link, isCatalog && css.active)}
+                href="/catalog"
+                aria-label="Catalog"
+              >
+                Catalog
+              </Link>
+            </li>
+          </ul>
+        </nav>
+      </div>
+    </header>
+  );
 }
