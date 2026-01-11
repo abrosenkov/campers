aeb1037986f3eb19386a5b117f7f9263ecb77c6c
abrosenkov <abrosenkov91@gmail.com>
2026-01-11 22:46:29 +0100

    fix img

--- components/Catalog/CamperCard/CamperCard.tsx
+++ components/Catalog/CamperCard/CamperCard.tsx
@@ -69,7 +69,6 @@ export default function CamperCard({ camper }: CamperCardProps) {
           alt={camper.name}
           width={292}
           height={320}
-          style={{ height: "auto" }}
           className={css.image}
         />
       </div>
