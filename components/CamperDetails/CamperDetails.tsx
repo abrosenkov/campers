57e9d58e241be73fadc4176f7ae83e57974b5146
abrosenkov <abrosenkov91@gmail.com>
2026-01-14 20:43:18 +0100

    add lightbox for images

--- components/CamperDetails/CamperDetails.tsx
+++ components/CamperDetails/CamperDetails.tsx
@@ -5,12 +5,21 @@ import css from "./CamperDetails.module.css";
 
 import { Camper } from "@/types";
 import CamperTabs from "./CamperTabs/CamperTabs";
+import { useState } from "react";
+import Lightbox from "yet-another-react-lightbox";
+import "yet-another-react-lightbox/styles.css";
 
 type CamperDetailsProps = {
   camper: Camper;
 };
 
 export default function CamperDetails({ camper }: CamperDetailsProps) {
+  const [index, setIndex] = useState(-1);
+
+  const slides = camper.gallery.map((item) => ({
+    src: item.original || item.thumb,
+  }));
+
   return (
     <div className={css.mainSection}>
       <div className={css.infoWrap}>
@@ -36,7 +45,7 @@ export default function CamperDetails({ camper }: CamperDetailsProps) {
 
       <ul className={css.gallery}>
         {camper.gallery.map((item, i) => (
-          <li key={i} className={css.itemImg}>
+          <li key={i} onClick={() => setIndex(i)} className={css.itemImg}>
             <Image
               src={item.thumb}
               alt={camper.name}
@@ -46,6 +55,14 @@ export default function CamperDetails({ camper }: CamperDetailsProps) {
           </li>
         ))}
       </ul>
+
+      <Lightbox
+        index={index}
+        open={index >= 0}
+        close={() => setIndex(-1)}
+        slides={slides}
+        controller={{ closeOnBackdropClick: true }}
+      />
 
       <p className={css.description}>{camper.description}</p>
       <CamperTabs camper={camper} />
